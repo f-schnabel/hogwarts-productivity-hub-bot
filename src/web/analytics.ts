bdb690a1d991b77c3c793109fd623ab571e9fe47
Felix Schnabel <f.schnabel@tum.de>
2026-01-29 11:39:45 +0100

    Fix build

--- src/web/analytics.ts
+++ src/web/analytics.ts
@@ -3,7 +3,7 @@ import registerIndexRoute from "./routes/index.ts";
 import registerLeaderboardRoute from "./routes/leaderboard.ts";
 import registerUserIdRoute from "./routes/user_id.ts";
 
-export const analyticsRouter = Router();
+export const analyticsRouter: Router = Router();
 registerIndexRoute(analyticsRouter);
 registerLeaderboardRoute(analyticsRouter);
 registerUserIdRoute(analyticsRouter);
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,13 @@
 /** @jest-config-loader ts-node */
 import { defineConfig } from "vitest/config";
+import path from "path";
 
 const config = defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dirname, "./src"),
+    },
+  },
   test: {
     globals: true,
     environment: "node",
